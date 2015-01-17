78dd68eb1a796e94e020d921d6947451d8dbc407
Nezz <bloodsource@vipmail.hu>
2015-01-17 17:55:16 +0100

    Changes for Xamarin Unified API

--- src/ServiceStack.Text/Marc/TypeAccessor.cs
+++ src/ServiceStack.Text/Marc/TypeAccessor.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Reflection;
-using System.Reflection.Emit;
+//using System.Reflection.Emit;
 using System.Threading;
 //using System.Dynamic;
 
